d062fb4ec089d52022857d3ad5f73430b9e54e16
aman2418 <aman.jaiswal2418@gmail.com>
2022-12-04 20:53:00 +0100

    Changed the file to print in different color snowflake

--- snowflake/let_it_snow.py
+++ snowflake/let_it_snow.py
@@ -6,15 +6,14 @@ def main(speed=0, bg_color="grey"):
     # create Turtle object
     turtle_screen = turtle.Screen()
     myTurtle = turtle.Turtle()
-    
+
     # set speed to 'fastest = 0'
     myTurtle.speed(speed)
     # change background color
     turtle_screen.bgcolor(bg_color)
-  
- 
-    """TODO: define different colors here"""
 
+    """TODO: define different colors here"""
+    colors = ["white","red","black","violet","cyan","green","pink","purple" ]
 
     for _ in range(10):
         # define some params
@@ -22,7 +21,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
+        myTurtle.color(colors[np.random.randint(0,len(colors))])
         # Go to the start position of the snowflake
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
